d133610da7f9b4e4158cfd8af2adcb12e565d380
unknown <am23am45@naver.com>
2026-04-03 11:03:01 +0900

    refactor(argus-agent): delete old metrics struct not used.

--- argus-agent/internal/collector/collector.go
+++ argus-agent/internal/collector/collector.go
@@ -1,18 +1,8 @@
 package collector
 
 import (
-	"time"
-
 	"github.com/noboaki/argus-agent/domain"
 )
-
-type Metrics struct {
-	Timestamp time.Time
-
-	CPUUsage  float64
-	MemUsage  float64
-	DiskUsage float64
-}
 
 type Collector interface {
 	Collect() (*domain.ArgusMetric, error)
